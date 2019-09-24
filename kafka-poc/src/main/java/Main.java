960342c0764bfdf6c1d16667b4ac68968c223623
TimmannaC <43141919+TimmannaC@users.noreply.github.com>
2019-09-24 22:16:19 +0530

    Update Main.java

--- kafka-poc/src/main/java/Main.java
+++ kafka-poc/src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         ExecutorService service = Executors.newFixedThreadPool(Integer.parseInt(args[0]));
-        File path = new File("C:\\Users\\Abzooba\\Downloads\\sample-data");
+        File path = new File("C:\\Users\\Downloads\\sample-data");
 
         for (File fileName :path.listFiles()) {
             System.out.println("Reading the fileName : "+fileName.toString());
